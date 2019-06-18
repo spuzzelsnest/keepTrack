fce127bb1523327085ea02a5da9be29b683db19d
spuzzelsnest <j.mpdesmet@gmail.com>
2019-06-19 01:46:10 +0200

    whos the king

--- src/clt/app/app-routing.module.ts
+++ src/clt/app/app-routing.module.ts
@@ -23,20 +23,12 @@ const routes: Routes = [
     data: { title: 'Log Details' }
   },
   {
-<<<<<<< HEAD
     path: 'add',
-=======
-    path: 'log/add',
->>>>>>> 209e556e85d5ef9c78556ecd6e27ba0871a68582
     component: LogAddComponent,
     data: { title: 'Log Add' }
   },
   {
-<<<<<<< HEAD
     path: 'edit/:id',
-=======
-    path: 'log/:id',
->>>>>>> 209e556e85d5ef9c78556ecd6e27ba0871a68582
     component: LogEditComponent,
     data: { title: 'Log Edit' }
   },
