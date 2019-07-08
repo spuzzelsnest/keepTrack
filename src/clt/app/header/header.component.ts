9f7ab6659a2ea48a40b9a4f592da170c029dc748
spuzzelsnest <j.mpdesmet@gmail.com>
2019-07-08 13:50:36 +0200

    restructured header for UserInfo

--- src/clt/app/header/header.component.ts
+++ src/clt/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, OnInit, Input} from "@angular/core";
+import { RestService } from '../rest.service';
+import { ActivatedRoute, RouterModule, Router, Params, ParamMap } from '@angular/router';
 
 @Component({
     selector: 'app-header',
@@ -7,4 +9,23 @@ import {Component} from "@angular/core";
 })
 
 export class HeaderComponent {
+    
+    @Input() userName: string;
+    
+    private key: string;
+    
+    constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) {}
+    
+    ngOnInit(){
+        this.route.params.subscribe(params => {
+             if (params['key']) {
+                
+                this.key = params.key;
+                console.log('KEY from header '+ this.key);
+                 
+             }else{
+                console.log('No Key found');
+             }
+        });
+    }
 }
