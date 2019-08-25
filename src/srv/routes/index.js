b011fb882a9ebef2c02be8dbf4264a345fc45ccc
spuzzelsnest <j.mpdesmet@gmail.com>
2019-08-26 00:57:09 +0200

    mime type issue in chrome

--- src/srv/routes/index.js
+++ src/srv/routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import path from 'path';
 
 import LogicController from '../controllers/logic';
 const router = new express.Router();
 
+//router.get(express.static(path.join(__dirname, '../_frontend')));
+router.get(express.static('../_frontend'));
 router.get('/api/login/:key', LogicController.getLogin);
 router.get('/api/:key/logs', LogicController.getLogs);
 router.post('/api/:key/add', LogicController.createLog);
